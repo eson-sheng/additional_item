71f86e0a8855d6f71a3f8c5eb29a1f1e2e543a8e
eson <834767372@qq.com>
2018-10-09 16:22:27 +0800

    【更新】- 日志查看系统说明书utf-8

--- log.viewer/logs.php
+++ log.viewer/logs.php
@@ -1,7 +1,14 @@
 <?php
 // 复制此文件到logs.php进行配置，eg：
-$logs = [
-    './aa/a.log',
-    './b.log',
-    '/Users/eson/www_nginx/app/mynote/mynote/basic/runtime/logs/logs.txt',
+$paths = [
+	'/home/www/mynote/basic/runtime/logs/.api_info.log',//接口请求信息
+	'/home/www/mynote/basic/runtime/logs/.curl_error.log',//curl 模块错误消息
+        '/home/www/mynote/basic/runtime/logs/.shell.log',//shell 执行日志
+	'/home/www/mynote/basic/runtime/logs/app.log',//mynote 项目错误日志
+	'/home/www/mynote/basic/runtime/logs/ct.txt',//ct 日志
+        '/usr/local/mariadb/var/mariadb.err',//mysql 错误日志
+        '/tmp/mysql.log',//mysql 查询日志
 ];
+
+$read_size = 50000;
+
