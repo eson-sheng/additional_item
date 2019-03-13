4312417640264ac9086b466fccd2571eb85c5ca9
yuebin.chen <cyb@piaojuke.com>
2019-03-13 13:45:41 +0800

    bug fix：添加common.php 的引用

--- log_analysis/index.php
+++ log_analysis/index.php
@@ -11,6 +11,9 @@ require_once __DIR__ . "/bin/autoload.php";
 /*配置文件加载*/
 $config = require __DIR__ . '/../config.php';
 
+$ct_log_path = __DIR__ . '/logs/log.txt';
+$config = require __DIR__ . '/../common.php';
+
 use Analysis\Analysis;
 
 $index = new Analysis($g_config);
--- log_storage/index.php
+++ log_storage/index.php
@@ -10,6 +10,9 @@ require_once __DIR__ . "/bin/autoload.php";
 /*配置文件加载*/
 $config = require __DIR__ . '/../config.php';
 
+$ct_log_path = __DIR__ . '/logs/log.txt';
+$config = require __DIR__ . '/../common.php';
+
 use Storage\Storage;
 
 $index = new Storage($g_config);
